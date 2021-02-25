9e117f7941ca480fa3da0bee807c6f30255aae47
Ivan Atanasov <iatanasov@tutanota.com>
2021-02-25 22:58:36 +0200

    Promises creation

--- src/app/App.js
+++ src/app/App.js
@@ -1,34 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 import HttpService from '../services/http-service';
+import React, {Component} from 'react';
 
 
 const http = new HttpService();
 
-function App() {
+// function App() {
 
-  http.getProducts();    
+class App  extends Component {
+  
+  constructor(props){
+    super(props);
+    this.loadData = this.loadData.bind(this);
+    this.loadData();
+  }
 
+  loadData = () => {
+    http.getProducts().then(data => {
+      console.log(data);
+    }, err => {
+      console.log("error");
+    });
+  }
+  
 
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> to create your swag shop.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> to create your swag shop.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
